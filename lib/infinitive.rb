2afba1663b64531f2428f82425ae3cc3f7ee430b
shuyo <nakatani.shuyo@gmail.com>
2009-02-03 15:15:45 +0900

    lib

---
+++ lib/infinitive.rb
@@ -0,0 +1,43 @@
+#!/usr/bin/ruby
+
+require 'rubygems'
+require 'stemmer'
+require 'linguistics'
+Linguistics::use( :en )
+
+class Infinitive
+  @@dir = caller(0)[0].sub(/\/[^\/]*:\d+$/,"")
+  def initialize(inflist_file = "#{@@dir}/inflist.txt", wordbook_file = "#{@@dir}/wordbook.txt")
+    @inflist = Hash.new
+    open(inflist_file) do |f|
+      while line = f.gets
+        @inflist[$1]=$2 if line =~ /^(.+)\t(.+)\n/
+      end
+    end
+    @infcache = Hash.new
+
+    @wordbook = Hash.new
+    open(wordbook_file) do |f|
+      while line = f.gets
+        @wordbook[line.chomp.stem.downcase]=0 if line !~ /^\s+$|^\s*#/
+      end
+    end
+  end
+
+  def inf(src)
+    return @infcache[src] if @infcache.key?(src)
+    st = @inflist[src] || src.en.infinitive
+    @infcache[src] = (if st == "" then src else st end).stem
+  end
+
+  def infinitive(word)
+    st = word2 = word.downcase
+    st = word2.stem
+    if @wordbook.key?(st)
+      st
+    else
+      inf(word2) || st
+    end
+  end
+end
+
