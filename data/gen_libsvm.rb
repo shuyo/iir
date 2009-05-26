b9b1555d929c5d78b0d9fe213aaf995bab62fe7e
shuyo <nakatani.shuyo@gmail.com>
2009-05-26 16:52:48 +0900

    complete gen_libsvm.rb

--- data/gen_libsvm.rb
+++ data/gen_libsvm.rb
@@ -10,40 +10,64 @@ require "../lib/extract_gutenberg.rb"
 require '../lib/infinitive.rb'
 INF = Infinitive.new
 
-def extract_words(path, terms)
-	file = $1 if path =~ /\/([^\/]+)$/
-	text = if path =~ /\.zip$/i
-	  file.sub!(/\.zip$/i, ".txt")
-	  `unzip -cq #{path} "*.txt"`
-	else
-	  open(path){|f| f.read}
-	end
-  text = Gutenberg.extract(text)
-
-  list = text.split(/^[IVX]+\s*\.?$/)[1..-1]
-  list = text.split(/^\n{4}$/) if list.size<=1
-  docs = Array.new
-  list.each do |x|
-    next unless x =~ /^(.+)$/
-    title = $1
-
-    words = x.scan(/[A-Za-z]+(?:'t)?/)
-    next if words.size < 1000
-
-    n = 0
-    doc_id = docs.size
-    words.each do |word|
-      word = INF.infinitive(word)
-      terms[word] ||= Hash.new(0)
-      terms[word][doc_id] += 1
-      n += 1
-    end
-    docs << {:title=>title, :n_words=>n}
+REG_TITLE = /^([A-Z][A-Z\-', ]+)$/
+class Analyzer
+  def initialize
+    @terms = Hash.new
+    @docs = Array.new
   end
-  docs
+  attr_reader :docs, :terms
+  def extract_words(path, sign)
+    file = path.dup
+  	file = $1 if path =~ /\/([^\/]+)$/
+  	text = if path =~ /\.zip$/i
+  	  file.sub!(/\.zip$/i, ".txt")
+  	  `unzip -cq #{path} "*.txt"`
+  	else
+  	  open(path){|f| f.read}
+  	end
+    text = Gutenberg.extract(text)
+
+    list = text.split(REG_TITLE)
+
+    title = nil
+    list.each do |x|
+      if x =~ REG_TITLE
+        title = x
+        next
+      end
+      words = x.scan(/[A-Za-z]+(?:'t)?/)
+      next if words.size < 1000
+
+      while words.size >= 100
+        subwords = words.slice!(0, 100)
+        n = 0
+        doc_id = @docs.size
+        subwords.each do |word|
+          word = INF.infinitive(word)
+          @terms[word] ||= Hash.new(0)
+          @terms[word][doc_id] += 1
+          n += 1
+        end
+        @docs << {:title=>title, :n_words=>n, :sign=>sign}
+      end
+    end
+  end
 end
 
-terms = Hash.new
-positive_docs = extract_words(ARGV[0], terms)
-negative_docs = extract_words(ARGV[1], terms)
+ana = Analyzer.new
+ana.extract_words ARGV[0], "+1"
+ana.extract_words ARGV[1], "-1"
+
+words = ana.terms.keys
+ana.docs.each_with_index do |doc, doc_id|
+  buf = [doc[:sign]]
+  words.each_with_index do |word, word_id|
+    if ana.terms[word]
+      freq = ana.terms[word][doc_id]
+      buf << "#{word_id}:#{freq}" if freq>0
+    end
+  end
+  puts buf.join(' ')
+end
 
