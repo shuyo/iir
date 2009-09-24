9a85a26d9afd46a31fa8f1e534f2a2843d7adecb
shuyo <nakatani.shuyo@gmail.com>
2009-09-24 18:43:51 +0900

    update (slightly fast)

--- irt/irt.rb
+++ irt/irt.rb
@@ -32,28 +32,19 @@ Zlib::GzipReader.open('word_scores.txt.gz') do |f|
   end
 end
 
-def error_function(data, users, words)
-  e = 0
-  data.each do |user_id, word_id, t|
-    z = users[user_id] - words[word_id]
-    y = 1.0/(1.0+Math.exp(-z))
-    e -= if t==1 then Math.log(y) else Math.log(1-y) end
-  end
-  e
-end
-
-puts error_function(data, users, words)
 100.times do |k|
   eta = 1.0/(k+10)
+  e = 0
   data.sort_by{rand}.each do |user_id, word_id, t|
     z = users[user_id] - words[word_id]
     y = 1.0/(1.0+Math.exp(-z))
+    e -= if t==1 then Math.log(y) else Math.log(1-y) end
 
     grad_e_eta = eta*(y - t)
     users[user_id] -= grad_e_eta
     words[word_id] += grad_e_eta
   end
-  puts "#{k}: #{error_function(data, users, words)}"
+  puts "#{k}: #{e}"
 end
 
 open(learnfile, 'w'){|f| Marshal.dump([users,words], f) }
