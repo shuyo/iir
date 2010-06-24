9331ae59d27bc5c7c5b7189e848773c0ad24cdff
shuyo <nakatani.shuyo@gmail.com>
2010-06-24 13:30:24 +0900

    optimize

--- sequence/crf.py
+++ sequence/crf.py
@@ -26,89 +26,90 @@ def load_text(filename, limit=1e9):
     f.close()
     return (text, labels)
 
-class CRF(object):
-    def __init__(self):
+class FeatureVector(object):
+    def __init__(self, features, ylist, xlist):
+        '''statistics of features (sufficient statistics like)'''
+        flist = features.features
+        glist = features.features_edge
+        self.K = len(features.labels)
+
+        self.Fss = numpy.zeros(len(flist), dtype=int)
+        for y1, x1 in zip(ylist, xlist):
+            self.Fss += [f(x1, y1) for f in flist]
+
+        self.Gss = numpy.zeros(len(glist), dtype=int)
+        for y1, y2 in zip(["start"] + ylist, ylist + ["end"]):
+            self.Gss += [g(y1, y2) for g in glist]
+
+        self.Fmat = []
+        self.Gmat = []
+        for x in xlist:
+            mt = numpy.zeros((len(flist), self.K), dtype=int)
+            for j, f in enumerate(flist):
+                mt[j] = [f(x, y) for y in features.labels]
+            self.Fmat.append(mt)
+
+            gmlist = []
+            for y1 in features.labels:
+                mt = numpy.zeros((len(glist), self.K), dtype=int)
+                for j, g in enumerate(glist):
+                    mt[j] = [g(y1, y2) for y2 in features.labels]
+                gmlist.append(mt)
+            self.Gmat.append(gmlist)
+
+    def cost(self, theta_f, theta_g):
+        return numpy.dot(theta_f, self.Fss) + numpy.dot(theta_g, self.Gss)
+
+    def logMlist(self, theta_f, theta_g):
+        Mlist = []
+        for fm, gmlist in zip(self.Fmat, self.Gmat):
+            gv = numpy.zeros((self.K, self.K))
+            for j, gm in enumerate(gmlist):
+                gv[j] = numpy.dot(theta_g, gm)
+            Mlist.append(gv + numpy.dot(theta_f, fm))
+        return Mlist
+
+
+class Features(object):
+    def __init__(self, labels):
         self.features = []
         self.features_edge = []
-        self.labels = ["start","stop","H","B","F"] #,"CR","I"]
+        self.labels = ["start","stop"] + dict(map(lambda i: (i,1),labels)).keys()
+    def start_label_index(self):
+        return 0
+    def stop_label_index(self):
+        return 1
+    def size(self):
+        return len(self.features)
+    def size_edge(self):
+        return len(self.features_edge)
 
     def add_feature(self, f):
         self.features.append(f)
     def add_feature_edge(self, f):
         self.features_edge.append(f)
 
-    def init_param(self):
-        #self.theta = (numpy.random.rand(len(self.features)) - 0.5)
-        #self.theta_edge = (numpy.random.rand(len(self.features_edge)) - 0.5)
-        self.theta = numpy.random.randn(len(self.features))
-        self.theta_edge = numpy.random.randn(len(self.features_edge))
-        return numpy.append(self.theta, self.theta_edge)
+class CRF(object):
+    def __init__(self, features):
+        self.features = features
 
-    def set_param(self, theta):
-        self.theta = theta[:len(self.theta)]
-        self.theta_edge = theta[len(self.theta):]
+    def random_param(self):
+        return numpy.random.randn(self.features.size()+ self.features.size_edge())
 
-    def feature(self, x, y):
-        energy = 0
-        for x_i, y_i in zip(x, y):
-            fv = [f(x_i, y_i) for f in self.features]
-            energy += numpy.dot(self.theta, fv)
-        print energy
+    def likelihood(self, fv, theta):
+        theta_f = theta[:self.features.size()]
+        theta_g = theta[self.features.size():]
 
-    def logM(self, i, x):
-        '''calculate matrix M_i(x). (i=1,...,n-1)'''
-        K = len(self.labels) # number of labels(states)
-        logM = numpy.zeros((K, K))
-        for k2, y2 in enumerate(self.labels):
-
-            fv = [f(x[i], y2) for f in self.features]
-            fv = numpy.dot(self.theta, fv)
-
-            for k1, y1 in enumerate(self.labels):
-                gv = [g(y1, y2) for g in self.features_edge]
-                logM[k1, k2] = numpy.dot(self.theta_edge, gv) + fv
-        return logM
-
-    def cost(self, y, x):
-        F = numpy.zeros(len(self.features), dtype=int)
-        for y1, x1 in zip(y, x):
-            F += [f(x1, y1) for f in self.features]
-        G = numpy.zeros(len(self.features_edge), dtype=int)
-        for y1, y2 in zip(["start"] + y, y + ["end"]):
-            G += [g(y1, y2) for g in self.features_edge]
-        return numpy.dot(self.theta, F) + numpy.dot(self.theta_edge, G)
-
-    def likelihood(self, labels, text, theta=None):
-        if theta!=None: self.set_param(theta)
-
-        Ms = [self.logM(0, text)]
-        logalpha = Ms[0][self.labels.index("start")] # alpha(1)
-        #alpha = [1 if x=="start" else 0 for x in self.labels]
-        #alpha = numpy.dot(alpha, numpy.exp(Ms[0]))
-        #print numpy.exp(logalpha) / alpha - 1
-        for i in range(1, len(text)): # n-2
-            logM = self.logM(i, text)
-            Ms.append(logM)
+        Mlist = fv.logMlist(theta_f, theta_g)
+        logalpha = Mlist[0][self.features.start_label_index()] # alpha(1)
+        for logM in Mlist[1:]: # n-2
             logalpha = logdotexp_vec_mat(logalpha, logM)
-            #alpha = numpy.dot(alpha, numpy.exp(logM))
-            #print numpy.exp(logalpha) / alpha - 1
 
-        #for m in Ms: print m
+        #logbeta = Mlist[-1][:, self.features.stop_label_index()]
+        #for logM in Mlist[-2::-1]:
+        #    logbeta = logdotexp_mat_vec(logM, logbeta)
 
-        logbeta = Ms[-1][:, self.labels.index("stop")]
-        #beta = [1 if x=="stop" else 0 for x in self.labels]
-        #beta = numpy.dot(numpy.exp(Ms[-1]), beta)
-        #print numpy.exp(logbeta) / beta - 1
-
-        for logM in Ms[-2::-1]:
-            logbeta = logdotexp_mat_vec(logM, logbeta)
-            #beta = numpy.dot(numpy.exp(logM), beta)
-            #print numpy.exp(logbeta) / beta - 1
-
-        #print self.labels
-        #print "Z(X) = ", logalpha[self.labels.index("stop")], logbeta[self.labels.index("start")]
-        #print logalpha / logbeta
-        return self.cost(labels, text) - logalpha[self.labels.index("stop")]
+        return fv.cost(theta_f, theta_g) - logalpha[self.features.stop_label_index()]
 
 def logdotexp_vec_mat(loga, logM):
     return numpy.array([maxentropy.logsumexp(loga + x) for x in logM.T], copy=False)
@@ -126,38 +127,43 @@ def main():
     if not options.filename: parser.error("need corpus filename(-f)")
 
     text, labels = load_text(options.filename)
-    #print dict(map(lambda i: (i,1),labels)).keys()
 
-    crf = CRF()
-    for label in crf.labels:
+    features = Features(labels)
+    for label in features.labels:
         for keyword in "project gutenberg etext copyright chapter".split():
-            crf.add_feature( lambda x, y: 1 if re.search(keyword, x, re.I) and y == label else 0 )
-        crf.add_feature( lambda x, y: 1 if y == label else 0 )
-        crf.add_feature_edge( lambda y_, y: 1 if y_ == label else 0 )
+            features.add_feature( lambda x, y: 1 if re.search(keyword, x, re.I) and y == label else 0 )
+        features.add_feature( lambda x, y: 1 if y == label else 0 )
+        features.add_feature_edge( lambda y_, y: 1 if y_ == label else 0 )
 
-    crf.add_feature( lambda x, y: 1 if re.search(r"[A-Z]{3}", x) else 0 )
-    crf.add_feature( lambda x, y: 1 if re.search(r"[0-9]", x) else 0 )
-    crf.add_feature( lambda x, y: 1 if x.find("@")>=0 else 0 )
-    crf.add_feature( lambda x, y: 1 if x.find("#")>=0 else 0 )
-    crf.add_feature( lambda x, y: 1 if x.startswith("*") else 0 )
-    crf.add_feature( lambda x, y: 1 if x.startswith("  ") else 0 )
-    crf.add_feature( lambda x, y: 1 if x.startswith("    ") else 0 )
-    crf.add_feature( lambda x, y: 1 if x.startswith("        ") else 0 )
-    crf.add_feature( lambda x, y: 1 if len(x.strip()) == 0 else 0 )
+    features.add_feature( lambda x, y: 1 if re.search(r"[A-Z]{3}", x) else 0 )
+    features.add_feature( lambda x, y: 1 if re.search(r"[0-9]", x) else 0 )
+    features.add_feature( lambda x, y: 1 if x.find("@")>=0 else 0 )
+    features.add_feature( lambda x, y: 1 if x.find("#")>=0 else 0 )
+    features.add_feature( lambda x, y: 1 if x.startswith("*") else 0 )
+    features.add_feature( lambda x, y: 1 if x.startswith("  ") else 0 )
+    features.add_feature( lambda x, y: 1 if x.startswith("    ") else 0 )
+    features.add_feature( lambda x, y: 1 if x.startswith("        ") else 0 )
+    features.add_feature( lambda x, y: 1 if len(x.strip()) == 0 else 0 )
 
-    #crf.add_feature_edge( lambda y_, y: 1 if len(x_.strip()) == 0 else 0 )
-    crf.add_feature_edge( lambda y_, y: 1 if y == y_ else 0 )
+    #features.add_feature_edge( lambda y_, y: 1 if len(x_.strip()) == 0 else 0 )
+    features.add_feature_edge( lambda y_, y: 1 if y == y_ else 0 )
 
-    theta = crf.init_param()
-    #crf.feature(text[0:20], labels[0:20])
+    fv = FeatureVector(features, labels, text)
 
-    likelihood = lambda x:-crf.likelihood(labels, text, x)
-    print -likelihood(theta)
+    crf = CRF(features)
+    likelihood = lambda x:-crf.likelihood(fv, x)
+
+    minL = 1e9
+    for i in range(10):
+        theta1 = crf.random_param()
+        L = likelihood(theta1)
+        print L
+        if minL > L:
+            minL = L
+            theta = theta1
+    print likelihood(theta), theta
     theta = optimize.fmin_bfgs(likelihood, theta)
-    print -likelihood(theta)
-
-
-
+    print likelihood(theta), theta
 
 
 if __name__ == "__main__":
