15639ce84639ee158c1507f51b05bcd5599626cb
shuyo <nakatani.shuyo@gmail.com>
2010-06-28 19:57:08 +0900

    support multisequence

--- sequence/crf.py
+++ sequence/crf.py
@@ -2,13 +2,9 @@
 # -*- coding: utf-8 -*-
 # Conditional Random Field
 
-import sys, os
-#from optparse import OptionParser
-
 import numpy
 from scipy import optimize, maxentropy
 numpy.set_printoptions(precision=5)
-
 
 def logdotexp_vec_mat(loga, logM):
     return numpy.array([maxentropy.logsumexp(loga + x) for x in logM.T], copy=False)
@@ -25,11 +21,12 @@ class FeatureVector(object):
 
         # expectation of features under empirical distribution (if ylist is specified)
         if ylist:
-            self.Fss = numpy.zeros(len(flist) + len(glist), dtype=int)
+            self.Fss = numpy.zeros(features.size(), dtype=int)
             for y1, y2 in zip(["start"] + ylist, ylist + ["end"]):
                 self.Fss[:len(flist)] += [f(y1, y2) for f in flist]
             for y1, x1 in zip(ylist, xlist):
                 self.Fss[len(flist):] += [g(x1, y1) for g in glist]
+            print self.Fss
 
         # index list of ON values of edge features
         self.Fon = [] # (n, #f, indexes)
@@ -71,28 +68,17 @@ class FeatureVector(object):
             fv[j] = numpy.dot(theta_f, fm)
         return [fv + numpy.dot(theta_g, gm) for gm in self.Gmat] + [fv]
 
-    def logMlist2(self, theta_f, theta_g):
-        '''for dependent fmlists on x_i'''
-        Mlist = []
-        for gm, fmlist in zip(self.Gmat, self.Fmat):
-            fv = numpy.zeros((self.K, self.K))
-            for j, fm in enumerate(fmlist):
-                fv[j] = numpy.dot(theta_f, fm)
-            Mlist.append(fv + numpy.dot(theta_g, gm))
-        return Mlist
-
-
 class Features(object):
     def __init__(self, labels):
         self.features = []
         self.features_edge = []
-        self.labels = ["start","stop"] + dict(map(lambda i: (i,1),labels)).keys()
+        self.labels = ["start","stop"] + dict([(i,1) for y in labels for i in y]).keys()
     def start_label_index(self):
         return 0
     def stop_label_index(self):
         return 1
     def size(self):
-        return len(self.features)
+        return len(self.features) + len(self.features_edge)
     def size_edge(self):
         return len(self.features_edge)
     def id2label(self, list):
@@ -119,7 +105,7 @@ class CRF(object):
             self.regularity_deriv = lambda w:numpy.sum(w) / v
 
     def random_param(self):
-        return numpy.random.randn(self.features.size()+ self.features.size_edge())
+        return numpy.random.randn(self.features.size())
 
     def logalphas(self, Mlist):
         logalpha = Mlist[0][self.features.start_label_index()] # alpha(1)
@@ -137,46 +123,55 @@ class CRF(object):
             logbetas.append(logbeta)
         return logbetas[::-1]
 
-    def likelihood(self, fv, theta):
+    def likelihood(self, fvs, theta):
         '''conditional log likelihood log p(Y|X)'''
         n_fe = self.features.size_edge() # number of features on edge
-        logMlist = fv.logMlist(theta[:n_fe], theta[n_fe:])
+        t1, t2 = theta[:n_fe], theta[n_fe:]
+        stop_index = self.features.stop_label_index()
 
-        log_Z = self.logalphas(logMlist)[-1][self.features.stop_label_index()]
-        return fv.cost(theta)  - self.regularity(theta) - log_Z
+        likelihood = 0
+        for fv in fvs:
+            logMlist = fv.logMlist(t1, t2)
+            logZ = self.logalphas(logMlist)[-1][stop_index]
+            likelihood += fv.cost(theta) - logZ
+        return likelihood - self.regularity(theta)
 
-    def gradient_likelihood(self, fv, theta):
+    def gradient_likelihood(self, fvs, theta):
         n_fe = self.features.size_edge() # number of features on edge
-        logMlist = fv.logMlist(theta[:n_fe], theta[n_fe:])
-        logalphas = self.logalphas(logMlist)
-        logbetas = self.logbetas(logMlist)
-        log_Z = logalphas[-1][self.features.stop_label_index()]
+        t1, t2 = theta[:n_fe], theta[n_fe:]
+        stop_index = self.features.stop_label_index()
+        start_index = self.features.start_label_index()
 
-        grad = numpy.array(fv.Fss, dtype=float) # empirical expectation
+        grad = numpy.zeros(self.features.size())
+        for fv in fvs:
+            logMlist = fv.logMlist(t1, t2)
+            logalphas = self.logalphas(logMlist)
+            logbetas = self.logbetas(logMlist)
+            logZ = logalphas[-1][stop_index]
 
-        expect_edge = numpy.zeros_like(logMlist[0])
-        for i in range(len(logMlist)):
-            if i==0:
-                expect_edge[self.features.start_label_index()] += numpy.exp(logalphas[i] + logbetas[i+1] - log_Z)
-            elif i<len(logbetas)-1:
-                m = logMlist[i]
-                a = logalphas[i-1][:,numpy.newaxis]
-                b = logbetas[i+1]
-                expect_edge += numpy.exp(m + b + a - log_Z)
-            else:
-                expect_edge[:,self.features.stop_label_index()] += numpy.exp(logalphas[i-1] + logbetas[i] - log_Z)
-        for k, indexes in enumerate(fv.Fon[0]):
-            grad[k] -= numpy.sum(expect_edge.take(indexes))
+            grad += numpy.array(fv.Fss, dtype=float) # empirical expectation
 
-        for i, gm in enumerate(fv.Gmat):
-            p_yi = numpy.exp(logalphas[i] + logbetas[i+1] - log_Z)
-            grad[n_fe:] -= numpy.sum(gm * numpy.exp(logalphas[i] + logbetas[i+1] - log_Z), axis=1)
+            expect = numpy.zeros_like(logMlist[0])
+            for i in range(len(logMlist)):
+                if i == 0:
+                    expect[start_index] += numpy.exp(logalphas[i] + logbetas[i+1] - logZ)
+                elif i < len(logbetas) - 1:
+                    a = logalphas[i-1][:, numpy.newaxis]
+                    expect += numpy.exp(logMlist[i] + a + logbetas[i+1] - logZ)
+                else:
+                    expect[:, stop_index] += numpy.exp(logalphas[i-1] + logbetas[i] - logZ)
+            for k, indexes in enumerate(fv.Fon[0]):
+                grad[k] -= numpy.sum(expect.take(indexes))
+
+            for i, gm in enumerate(fv.Gmat):
+                p_yi = numpy.exp(logalphas[i] + logbetas[i+1] - logZ)
+                grad[n_fe:] -= numpy.sum(gm * numpy.exp(logalphas[i] + logbetas[i+1] - logZ), axis=1)
 
         return grad - self.regularity_deriv(theta)
 
-    def inference(self, fv, theta):
-        likelihood = lambda x:-self.likelihood(fv, x)
-        likelihood_deriv = lambda x:-self.gradient_likelihood(fv, x)
+    def inference(self, fvs, theta):
+        likelihood = lambda x:-self.likelihood(fvs, x)
+        likelihood_deriv = lambda x:-self.gradient_likelihood(fvs, x)
         return optimize.fmin_bfgs(likelihood, theta, fprime=likelihood_deriv)
 
     def tagging(self, fv, theta):
@@ -184,7 +179,7 @@ class CRF(object):
         logMlist = fv.logMlist(theta[:n_fe], theta[n_fe:])
 
         logalphas = self.logalphas(logMlist)
-        log_Z = logalphas[-1][self.features.stop_label_index()]
+        logZ = logalphas[-1][self.features.stop_label_index()]
 
         delta = logMlist[0][self.features.start_label_index()]
         argmax_y = []
@@ -196,45 +191,109 @@ class CRF(object):
         for a in reversed(argmax_y):
             Y.append(a[Y[-1]])
 
-        return numpy.exp(Y[0] - log_Z), Y[::-1]
+        return numpy.exp(Y[0] - logZ), Y[::-1]
+
+    def tagging_verify(self, fv, theta):
+        '''verification of tagging'''
+        n_fe = self.features.size_edge() # number of features on edge
+        logMlist = fv.logMlist(theta[:n_fe], theta[n_fe:])
+        N = len(logMlist) - 1
+        K = logMlist[0][0].size
+
+        ylist = [0] * N
+        max_p = -1e9
+        argmax_p = None
+        while True:
+            logp = logMlist[0][self.features.start_label_index(), ylist[0]]
+            for i in range(len(ylist)-1):
+                logp += logMlist[i+1][ylist[i], ylist[i+1]]
+            logp += logMlist[N][ylist[N-1], self.features.stop_label_index()]
+            print ylist, logp
+            if max_p < logp:
+                max_p = logp
+                argmax_p = ylist[:]
+
+            for k in range(N-1,-1,-1):
+                if ylist[k] < K-1:
+                    ylist[k] += 1
+                    break
+                ylist[k] = 0
+            else:
+                break
+        return max_p, argmax_p
+
 
 
 def main():
-    text = "Confidence in the pound is widely expected to take another sharp dive if trade figures for September , due for release tomorrow , fail to show a substantial improvement from July and August s near-record deficits .".split()
-    labels = "B O B I O O O O O B I I O B I O B O O O B B O O O O B I I O B I I B I I O".split()
+    data = """
+    This DT B-NP
+    temblor-prone JJ I-NP
+    city NN I-NP
+    dispatched VBD B-VP
+    inspectors NNS B-NP
+    , , O
+    firefighters NNS B-NP
+    and CC O
+    other JJ B-NP
+    earthquake-trained JJ I-NP
+    personnel NNS I-NP
+    to TO B-VP
+    aid VB I-VP
+    San NNP B-NP
+    Francisco NNP I-NP
+    . . O
+    """
+    texts = []
+    labels = []
+    text = []
+    data = "\n" + data + "\n"
+    for line in data.split("\n"):
+        line = line.strip()
+        if len(line) == 0:
+            if len(text)>0:
+                texts.append(text)
+                labels.append(label)
+            text = []
+            label = []
+        else:
+            token, info, chunk = line.split()
+            text.append((token, info))
+            label.append(chunk)
 
     features = Features(labels)
-    words = dict(map(lambda i: (i,1), text)).keys()
+    tokens = dict([(i[0],1) for x in texts for i in x]).keys()
+    print tokens
+    infos = dict([(i[1],1) for x in texts for i in x]).keys()
+    print infos
+
+    def gen_f1(label, token):
+        return lambda x, y: 1 if y == label and x[0] == token else 0
+    def gen_f2(label, info):
+        return lambda x, y: 1 if y == label and x[1] == info else 0
     for label in features.labels:
-        features.add_feature( lambda x, y: 1 if y == label else 0 )
-        features.add_feature( lambda x, y: 1 if y == label and x.istitle() else 0 )
-        features.add_feature( lambda x, y: 1 if y == label and x.isalpha() else 0 )
-        features.add_feature( lambda x, y: 1 if y == label and len(x) <= 2 else 0 )
-        features.add_feature( lambda x, y: 1 if y == label and len(x) <= 4 else 0 )
-        for word in words:
-            features.add_feature( lambda x, y: 1 if y == label and x == word else 0 )
+        for token in tokens:
+            features.add_feature( gen_f1(label, token) )
+        for info in infos:
+            features.add_feature( gen_f2(label, info) )
+    features.add_feature_edge( lambda y_, y: 0 )
 
-        features.add_feature_edge( lambda y_, y: 1 if y_ == label else 0 )
-        features.add_feature_edge( lambda y_, y: 1 if y_ == y else 0 )
-        for label2 in features.labels:
-            features.add_feature_edge( lambda y_, y: 1 if y == label and y_ == label2 else 0 )
-
-    fv = FeatureVector(features, text, labels)
+    fvs = [FeatureVector(features, x, y) for x, y in zip(texts, labels)]
+    fv = fvs[0]
 
     crf = CRF(features, 2)
     theta = crf.random_param()
 
     print "theta:", theta
-    print "log likelihood:", crf.likelihood(fv, theta)
+    print "log likelihood:", crf.likelihood(fvs, theta)
     prob, ys = crf.tagging(fv, theta)
     print "tagging:", prob, features.id2label(ys)
 
-    theta = crf.inference(fv, theta)
+    theta = crf.inference(fvs, theta)
 
     print "theta:", theta
-    print "log likelihood:", crf.likelihood(fv, theta)
+    print "log likelihood:", crf.likelihood(fvs, theta)
     prob, ys = crf.tagging(fv, theta)
-    print "tagging:", prob, features.id2label(ys)
+    print "tagging:", prob, zip(texts[0], labels[0], features.id2label(ys))
 
 if __name__ == "__main__":
     main()
