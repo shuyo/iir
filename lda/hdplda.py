e0873cf44808c614a90035de32366553e8ebdcc8
shuyo <nakatani.shuyo@gmail.com>
2011-03-01 19:37:35 +0900

    faster

--- lda/hdplda.py
+++ lda/hdplda.py
@@ -29,8 +29,8 @@ class HDPLDA:
         self.n_terms = 0
         self.n_tables = 0
 
-        self.m_k = numpy.zeros(K, dtype=int) # number of tables for each topic
-        self.n_k = [0] * K  # number of terms for each topic
+        self.m_k = numpy.zeros(K, dtype=int)  # number of tables for each topic
+        self.n_k = numpy.zeros(K, dtype=int)  # number of terms for each topic
 
         voca = vocabulary.Vocabulary(stopwords==0)
 
@@ -70,6 +70,41 @@ class HDPLDA:
 
         return voca
 
+    def inference(self):
+        for j, x_i in enumerate(self.x_ji):
+            for i in range(len(x_i)):
+                self.sampling_table(j, i)
+            for t in self.tables[j]:
+                self.sampling_k(j, t)
+
+    def worddist(self):
+        return [(self.n_kv[k] + self.base) / (self.n_k[k] + self.Vbase) for k in self.topics]
+
+    def perplexity(self):
+        phi = self.worddist()
+        phi.append(numpy.zeros(self.V) + 1.0 / self.V)
+        log_per = 0
+        N = 0
+        gamma_over_T_gamma = self.gamma / (self.n_tables + self.gamma)
+        for j, x_i in enumerate(self.x_ji):
+            p_k = numpy.zeros(self.m_k.size)    # topic dist for document 
+            for t in self.tables[j]:
+                k = self.k_jt[j][t]
+                p_k[k] += self.n_jt[j][t]
+            len_x_alpha = len(x_i) + self.alpha
+            p_k /= len_x_alpha
+            
+            p_k_parent = self.alpha / len_x_alpha
+            p_k += p_k_parent * (self.m_k / (self.n_tables + self.gamma))
+            
+            theta = [p_k[k] for k in self.topics]
+            theta.append(p_k_parent * gamma_over_T_gamma)
+
+            for v in x_i:
+                log_per -= numpy.log(numpy.inner([p[v] for p in phi], theta))
+            N += len(x_i)
+        return numpy.exp(log_per / N)
+
     def dump(self, disp_x=False):
         if disp_x: print "x_ji:", self.x_ji
         print "t_ji:", self.t_ji
@@ -80,6 +115,9 @@ class HDPLDA:
         print "m_k:", self.m_k
         print "tables:", self.tables
         print "topics:", self.topics
+
+
+    # internal methods from here
 
     # cache for faster calcuration
     def updated_n_tables(self):
@@ -107,89 +145,37 @@ class HDPLDA:
             N += 1
         return s
 
-    # n_??/m_? を用いて f_k を高速に計算
-    def f_k_x_ji_fast(self, k, v):
-        return (self.n_kv[k, v] + self.base) / (self.n_k[k] + self.Vbase)
-
-    def log_f_k_new_x_jt_fast2(self, n_jt, n_tv, n_kv = None, n_k = 0):
+    def log_f_k_new_x_jt(self, n_jt, n_tv, n_kv = None, n_k = 0):
         p = self.cur_log_V_base(n_k) - self.cur_log_V_base(n_k + n_jt)
         for (v_l, n_l) in n_tv:
             n0 = n_kv[v_l] if n_kv != None else 0
             p += self.cur_log_base(n0 + n_l) - self.cur_log_base(n0)
         return p
 
-    def count_n_jtv(self, j, t):
+    def count_n_jtv(self, j, t, k_old):
+        """count n_jtv and decrease n_kv for k_old"""
         x_i = self.x_ji[j]
         t_i = self.t_ji[j]
         n_jtv = dict()
-        n_jt = 0
         for i, t1 in enumerate(t_i):
             if t1 == t:
                 v = x_i[i]
+                self.n_kv[k_old, v] -= 1
                 if v in n_jtv:
                     n_jtv[v] += 1
                 else:
                     n_jtv[v] = 1
-                n_jt += 1
-        return (n_jt, n_jtv.items())
-
-    # sampling topic
-    # 新しいトピックの場合、パラメータの領域を確保
-    def sampling_topic(self, p_k):
-        drawing = numpy.random.multinomial(1, p_k / p_k.sum()).argmax()
-        # 新しいトピック
-        if drawing < len(self.topics):
-            k_new = self.topics[drawing]
-        else:
-            # 空きトピックIDを取得(あれば再利用)
-            K = self.m_k.size
-            for k_new in range(K):
-                if k_new not in self.topics: break
-            else:
-                # なければ新しいテーブルID
-                k_new = K
-                self.n_k.append(0)
-                self.m_k = numpy.resize(self.m_k, K + 1)
-                self.m_k[k_new] = 0
-                self.n_kv = numpy.resize(self.n_kv, (k_new+1, self.V))
-                self.n_kv[k_new, :] = numpy.zeros(self.V, dtype=int)
-            self.topics.append(k_new)
-        return k_new
-
-    # 客 x_ji を新しいテーブルに案内
-    # テーブルのトピック(料理)もサンプリング
-    def new_table(self, j, i, f_k):
-        # 空きテーブルIDを取得
-        T_j = self.n_jt[j].size
-        for t_new in range(T_j):
-            if t_new not in self.tables[j]: break
-        else:
-            # 新しいテーブルID
-            t_new = T_j
-            self.n_jt[j].resize(t_new+1) # self.n_jt[j].append(0)
-            self.k_jt[j].append(0)
-        self.tables[j].append(t_new)
-        self.n_tables += 1
-        self.updated_n_tables()
-
-        # sampling of k (新しいテーブルの料理(トピック))
-        p_k = [self.m_k[k] * f_k[k] for k in self.topics]
-        p_k.append(self.gamma_f_k_new_x_ji) # self.gamma * self.f_k_new_x_ji_fast()
-        k_new = self.sampling_topic(numpy.array(p_k, copy=False))
-
-        self.k_jt[j][t_new] = k_new
-        self.m_k[k_new] += 1
-
-        return t_new
+        return n_jtv.items()
 
 
     # sampling t (table) from posterior
-    def sampling_t(self, j, i):
+    def sampling_table(self, j, i):
         v = self.x_ji[j][i]
         tables = self.tables[j]
         t_old = self.t_ji[j][i]
         k_old = self.k_jt[j][t_old]
 
+        # decrease counters
         self.n_kv[k_old, v] -= 1
         self.n_k[k_old] -= 1
         self.n_jt[j][t_old] -= 1
@@ -205,10 +191,19 @@ class HDPLDA:
                 # 客がいなくなった料理(トピック)
                 self.topics.remove(k_old)
 
-        # sampling of t ( p(t_ji=t) を求める )
-        f_k = numpy.zeros(self.m_k.size)
-        for k in self.topics:
-            f_k[k] = self.f_k_x_ji_fast(k, v)
+        # sampling from posterior p(t_ji=t)
+        t_new = self.sampling_t(j, i, v, tables)
+
+        # increase counters
+        self.t_ji[j][i] = t_new
+        self.n_jt[j][t_new] += 1
+
+        k_new = self.k_jt[j][t_new]
+        self.n_k[k_new] += 1
+        self.n_kv[k_new, v] += 1
+
+    def sampling_t(self, j, i, v, tables):
+        f_k = (self.n_kv[:, v] + self.base) / (self.n_k + self.Vbase)
         p_t = [self.n_jt[j][t] * f_k[self.k_jt[j][t]] for t in tables]
         p_x_ji = numpy.inner(self.m_k, f_k) + self.gamma_f_k_new_x_ji
         p_t.append(p_x_ji * self.alpha_over_T_gamma)
@@ -217,39 +212,79 @@ class HDPLDA:
         p_t /= p_t.sum()
         drawing = numpy.random.multinomial(1, p_t).argmax()
         if drawing < len(tables):
-            t_new = tables[drawing]
+            return tables[drawing]
         else:
-            t_new = self.new_table(j, i, f_k)
+            return self.new_table(j, i, f_k)
 
-        # update counters
-        self.t_ji[j][i] = t_new
-        self.n_jt[j][t_new] += 1
+    # 客 x_ji を新しいテーブルに案内
+    # テーブルのトピック(料理)もサンプリング
+    def new_table(self, j, i, f_k):
+        # 空きテーブルIDを取得
+        T_j = self.n_jt[j].size
+        for t_new in range(T_j):
+            if t_new not in self.tables[j]: break
+        else:
+            # new table ID
+            t_new = T_j
+            self.n_jt[j].resize(t_new+1)
+            self.n_jt[j][t_new] = 0
+            self.k_jt[j].append(0)
+        self.tables[j].append(t_new)
+        self.n_tables += 1
+        self.updated_n_tables()
 
-        k_new = self.k_jt[j][t_new]
-        self.n_k[k_new] += 1
-        self.n_kv[k_new, v] += 1
+        # sampling of k (新しいテーブルの料理(トピック))
+        p_k = [self.m_k[k] * f_k[k] for k in self.topics]
+        p_k.append(self.gamma_f_k_new_x_ji)
+        k_new = self.sampling_topic(numpy.array(p_k, copy=False))
 
-    # sampling k (dish=topic) from posterior
+        self.k_jt[j][t_new] = k_new
+        self.m_k[k_new] += 1
+
+        return t_new
+
+    # sampling topic
+    # 新しいトピックの場合、パラメータの領域を確保
+    def sampling_topic(self, p_k):
+        drawing = numpy.random.multinomial(1, p_k / p_k.sum()).argmax()
+        if drawing < len(self.topics):
+            # existing topic
+            k_new = self.topics[drawing]
+        else:
+            # new topic
+            K = self.m_k.size
+            for k_new in range(K):
+                # recycle table ID, if a spare ID exists
+                if k_new not in self.topics: break
+            else:
+                # new table ID, if otherwise
+                k_new = K
+                self.n_k = numpy.resize(self.n_k, k_new + 1)
+                self.n_k[k_new] = 0
+                self.m_k = numpy.resize(self.m_k, k_new + 1)
+                self.m_k[k_new] = 0
+                self.n_kv = numpy.resize(self.n_kv, (k_new+1, self.V))
+                self.n_kv[k_new, :] = numpy.zeros(self.V, dtype=int)
+            self.topics.append(k_new)
+        return k_new
+
     def sampling_k(self, j, t):
+        """sampling k (dish=topic) from posterior"""
         k_old = self.k_jt[j][t]
+        n_jt = self.n_jt[j][t]
         self.m_k[k_old] -= 1
-        self.n_k[k_old] -= self.n_jt[j][t]
-        if self.m_k[k_old] > 0:
-            for v, t1 in zip(self.x_ji[j], self.t_ji[j]):
-                if t1 != t: continue
-                self.n_kv[k_old, v] -= 1
-        else:
+        self.n_k[k_old] -= n_jt
+        if self.m_k[k_old] == 0:
             self.topics.remove(k_old)
 
         # sampling of k
-        # 確率が小さくなりすぎるので log で保持。最大値を引いてからexp&正規化
-        n_jt, n_jtv = self.count_n_jtv(j, t)
+        n_jtv = self.count_n_jtv(j, t, k_old)
         K = len(self.topics)
         log_p_k = numpy.zeros(K+1)
         for i, k in enumerate(self.topics):
-            log_p_k[i] = self.log_f_k_new_x_jt_fast2(n_jt, n_jtv, self.n_kv[k, :], self.n_k[k]) + numpy.log(self.m_k[k])
-        log_p_k[K] = self.log_f_k_new_x_jt_fast2(n_jt, n_jtv) + numpy.log(self.gamma)
-        k_new = self.sampling_topic(numpy.exp(log_p_k - log_p_k.max()))
+            log_p_k[i] = self.log_f_k_new_x_jt(n_jt, n_jtv, self.n_kv[k, :], self.n_k[k]) + numpy.log(self.m_k[k])
+        log_p_k[K] = self.log_f_k_new_x_jt(n_jt, n_jtv) + numpy.log(self.gamma)
+        k_new = self.sampling_topic(numpy.exp(log_p_k - log_p_k.max())) # for too small
 
         # update counters
         self.k_jt[j][t] = k_new
@@ -258,39 +293,6 @@ class HDPLDA:
         for v, t1 in zip(self.x_ji[j], self.t_ji[j]):
             if t1 != t: continue
             self.n_kv[k_new, v] += 1
-
-    def inference(self):
-        for j, x_i in enumerate(self.x_ji):
-            for i in range(len(x_i)):
-                self.sampling_t(j, i)
-            for t in self.tables[j]:
-                self.sampling_k(j, t)
-
-    def worddist(self):
-        return [(self.n_kv[k] + self.base) / (self.n_k[k] + self.Vbase) for k in self.topics]
-
-    def perplexity(self):
-        phi = self.worddist()
-        phi.append(numpy.zeros(self.V) + 1.0 / self.V)
-        log_per = 0
-        N = 0
-        for j, x_i in enumerate(self.x_ji):
-            p_k = numpy.zeros(self.m_k.size)    # topic dist for document 
-            for t in self.tables[j]:
-                k = self.k_jt[j][t]
-                p_k[k] += self.n_jt[j][t]
-            p_k /= len(x_i) + self.alpha
-            
-            p_k_parent = self.alpha / (len(x_i) + self.alpha)
-            p_k += p_k_parent * (self.m_k / (self.n_tables + self.gamma))
-            
-            theta = [p_k[k] for k in self.topics]
-            theta.append(p_k_parent * (self.gamma / (self.n_tables + self.gamma)))
-
-            for v in x_i:
-                log_per -= numpy.log(numpy.inner([p[v] for p in phi], theta))
-            N += len(x_i)
-        return numpy.exp(log_per / N)
 
 
 def hdplda_learning(hdplda, iteration):
@@ -315,7 +317,6 @@ def main():
     if not (options.filename or options.corpus): parser.error("need corpus filename(-f) or corpus range(-c)")
     if options.seed != None:
         numpy.random.seed(options.seed)
-        print "seed = ", options.seed
 
     if options.filename:
         corpus = vocabulary.load_file(options.filename)
@@ -328,9 +329,9 @@ def main():
     print "corpus=%d words=%d alpha=%f gamma=%f base=%f initK=%d stopwords=%d" % (len(corpus), len(voca.vocas), options.alpha, options.gamma, options.base, options.K, options.stopwords)
     #hdplda.dump()
 
-    import cProfile
-    cProfile.runctx('hdplda_learning(hdplda, options.iteration)', globals(), locals(), 'hdplda.profile')
-    #hdplda_learning(hdplda, options.iteration)
+    #import cProfile
+    #cProfile.runctx('hdplda_learning(hdplda, options.iteration)', globals(), locals(), 'hdplda.profile')
+    hdplda_learning(hdplda, options.iteration)
 
     phi = hdplda.worddist()
     for k, phi_k in enumerate(phi):
