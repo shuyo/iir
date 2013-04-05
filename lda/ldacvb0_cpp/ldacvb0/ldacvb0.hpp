b48c28625a9bade596ea66ad5ff3976ff41d4059
Nakatani Shuyo <nakatani.shuyo@gmail.com>
2013-04-05 11:17:46 +0900

    model initialization and test

--- lda/ldacvb0_cpp/ldacvb0/ldacvb0.hpp
+++ lda/ldacvb0_cpp/ldacvb0/ldacvb0.hpp
@@ -19,11 +19,14 @@ namespace std {
 #include <regex>
 #endif
 #include <algorithm>
+#include <cassert>
 #include <string>
 #include <unordered_map>
+#include <vector>
 #include <cybozu/string.hpp>
 #include <cybozu/string_operation.hpp>
 #include <cybozu/mmap.hpp>
+#include <random>
 
 namespace cybozu {
 namespace ldacvb0 {
@@ -34,7 +37,13 @@ namespace ldacvb0 {
 std::regex rexword("(\\S+)/\\S+");
 
 /*
- id and counter of vocabulary
+
+*/
+typedef std::vector<double> Vec;
+typedef std::vector<Vec> Mat;
+
+/*
+id and counter of vocabulary
 */
 class IdCount {
 public:
@@ -44,8 +53,10 @@ public:
 	IdCount(size_t id_, int count_) : id(id_), count(count_) {}
 };
 
+
+
 /*
-	identify and count freaquency of vocabulary
+identify and count freaquency of vocabulary
 */
 template <class WORD>
 class Vocabularies {
@@ -57,10 +68,10 @@ public:
 	size_t add(const WORD &word) {
 		WORD key(word);
 		normalize(key);
-		if (voca.find(key) != voca.end()) {
-			IdCount& x = voca[key];
-			x.count += 1;
-			return x.id;
+		auto x = voca.find(key);
+		if (x != voca.end()) {
+			x->second.count += 1;
+			return x->second.id;
 		} else {
 			size_t new_id = vocalist.size();
 			voca[key] = IdCount(new_id, 1);
@@ -103,28 +114,45 @@ public:
 	}
 };
 
-typedef std::vector<size_t> Document;
+class Term {
+public:
+	size_t id;
+	int freq;
+	Term() : id(0), freq(0) {}
+	Term(size_t id_, int freq_) : id(id_), freq(freq_) {}
+};
+typedef std::vector<Term> Document;
 
 /*
-	doocument loader
+doocument loader
 */
 template <class STRING, class CHAR>
-class Documents : std::vector<Document> {
+class Documents : public std::vector<Document> {
 public:
 	Vocabularies<STRING> vocabularies;
 
 private:
 	template <class T> void addeachword(std::regex_iterator<T> i) {
-		push_back(Document());
-		Document& doc = back();
-
+		std::unordered_map<size_t, int> count;
 		std::regex_iterator<T> iend;
 		for (; i != iend; ++i) {
 			const std::string& w = (*i)[1].str();
 			char c = w[0];
 			if (c < 'A' || (c > 'Z' && c < 'a') || c > 'z') continue;
 			size_t id = vocabularies.add(w);
-			doc.push_back(id);
+			auto x = count.find(id);
+			if (x != count.end()) {
+				x->second += 1;
+			} else {
+				count[id] = 1;
+			}
+		}
+
+		push_back(Document());
+		Document& doc = back();
+		auto j = count.begin(), jend = count.end();
+		for (;j!=jend;++j) {
+			doc.push_back(Term(j->first, j->second));
 		}
 	}
 
@@ -150,15 +178,15 @@ public:
 
 
 /*
- call the procedure for each word (mmap)
- */
+call the procedure for each word (mmap)
+*/
 template <class T, class CHAR>
 void eachwords(const CHAR* p, const CHAR* end, T& func) {
 };
 
 /*
- call the procedure for each word (std::string, cybozu::String)
- */
+call the procedure for each word (std::string, cybozu::String)
+*/
 template <class T, class STRING>
 void eachwords(STRING st, T& func) {
 	std::regex_token_iterator<STRING::iterator> i( st.begin(), st.end(), rexword ), iend;
@@ -180,16 +208,199 @@ template <class T> void loadeachwords(std::string filename, T& func) {
 	}
 }
 
-class LDA_CVB0 {
+
+
+
+
+
+/*
+
+*/
+void update_for_word(
+	Vec& gamma_k,
+	Vec::iterator i_wk_buf, Vec::iterator i_jk_buf, Vec::iterator i_k_buf,
+	Vec::const_iterator& i_wk, Vec::const_iterator i_jk, Vec::const_iterator i_k,
+	const size_t w, const int freq, const int K
+	) {
+		i_wk += w * K;
+		i_wk_buf += w * K;
+
+		auto i_gamma = gamma_k.begin();
+		double sum_gamma = 0;
+		for (int k=0;k<K;++k) {
+			double gamma = *i_gamma;
+			double new_gamma = (*i_wk++ - gamma) * (*i_jk++ - gamma) / (*i_k++ - gamma);
+			*i_gamma++ = new_gamma;
+			sum_gamma += new_gamma;
+		}
+		i_gamma = gamma_k.begin();
+		for (int k=0;k<K;++k) {
+			double gamma = *i_gamma / sum_gamma;
+			*i_gamma++ = gamma;
+			gamma *= freq;
+			*i_wk_buf++ += gamma;
+			*i_jk_buf++ += gamma;
+			*i_k_buf++ += gamma;
+		}
+}
+
+
+void parameter_init(Vec& n_wk, Vec& n_jk, Vec& n_k, const Documents<std::string, char>& docs, const int K) {
+	const size_t M = docs.size();
+	const size_t V = docs.vocabularies.size();
+	n_wk.resize(V*K);
+	n_jk.resize(M*K);
+	n_k.resize(K);
+
+}
+
+class dirichlet_distribution {
 private:
-	int K_;
+	std::mt19937 generator;
+public:
+	dirichlet_distribution() {
+
+	}
+
+	dirichlet_distribution(unsigned long seed) {
+		if (seed>0) {
+			generator.seed(seed);
+		} else {
+			dirichlet_distribution();
+		}
+	}
+	void draw(Vec& vec, const int K, const double alpha) {
+		std::gamma_distribution<double> distribution(alpha, 1.0);
+		if (vec.size() != K) vec.resize(K);
+		double sum = 0;
+		auto i = vec.begin(), iend = vec.end();
+		for (;i!=iend;++i) {
+			double x = distribution(generator);
+			sum += x;
+			*i = x;
+		}
+		for (i=vec.begin();i!=iend;++i) {
+			*i /= sum;
+		}
+	}
+	void draw(Vec& vec, const Vec& alpha) {
+		if (vec.size() != alpha.size()) vec.resize(alpha.size());
+		double sum = 0;
+		auto i = vec.begin(), iend = vec.end();
+		auto a = alpha.begin();
+		for (;i!=iend;++i,++a) {
+			std::gamma_distribution<double> distribution(*a, 1.0);
+			double x = distribution(generator);
+			sum += x;
+			*i = x;
+		}
+		for (i=vec.begin();i!=iend;++i) {
+			*i /= sum;
+		}
+	}
+};
+
+/*
+
+*/
+class LDA_CVB0 {
+public:
+	int K_, V_;
 	double alpha_;
 	double beta_;
-public:
-	LDA_CVB0(int K, double alpha, double beta) :
-	  K_(K), alpha_(alpha), beta_(beta) {
+	Vec n_wk1, n_wk2, n_jk1, n_jk2, n_k1, n_k2;
+	Vec &n_wk, &n_wk_buf, &n_jk, &n_jk_buf, &n_k, &n_k_buf;
+	Mat gamma_jik;
+	const Documents<std::string, char>& docs_;
+	LDA_CVB0(int K, int V, double alpha, double beta, const Documents<std::string, char>& docs) :
+	K_(K), V_(V), alpha_(alpha), beta_(beta), docs_(docs),
+		n_wk(n_wk1), n_wk_buf(n_wk2), n_jk(n_jk1), n_jk_buf(n_jk2), n_k(n_k1), n_k_buf(n_k2) {
+			parameter_init(n_wk1, n_jk1, n_k1, docs, K);
+			parameter_init(n_wk2, n_jk2, n_k2, docs, K);
 
-	  }
+			std::fill(n_wk.begin(), n_wk.end(), beta_);
+			std::fill(n_jk.begin(), n_jk.end(), alpha_);
+			std::fill(n_k.begin(), n_k.end(), beta_ * V_);
+
+			cybozu::ldacvb0::dirichlet_distribution dd(1U);
+
+			auto j = docs_.begin(), jend = docs_.end();
+			auto j_jk = n_jk.begin();
+			for (;j!=jend;++j) {
+				auto i = j->begin(), iend = j->end();
+				for (;i!=iend;++i) {
+					size_t w = i->id;
+					int freq = i->freq;
+
+					Vec aph(K);
+					auto aend = aph.end();
+					double sum = 0;
+					{
+						auto i_wk = n_wk.begin() + w * K;
+						auto i_jk = j_jk;
+						auto i_k = n_k.begin();
+						for (auto ai = aph.begin();ai!=aend;++ai,++i_wk,++i_jk,++i_k) {
+							sum += *ai = *i_wk * *i_jk / *i_k;
+						}
+					}
+					sum = alpha / sum;
+					for (auto ai = aph.begin(); ai != aend; ++ai) *ai *= sum;
+
+					gamma_jik.push_back(Vec());
+					Vec& gamma = gamma_jik.back();
+					dd.draw(gamma, aph);
+
+					auto gi = gamma.begin(), gend = gamma.end();
+					auto i_wk = n_wk.begin() + w * K;
+					auto i_jk = j_jk;
+					auto i_k = n_k.begin();
+					for (;gi!=gend;++gi,++i_wk,++i_jk,++i_k) {
+						double g = *gi * freq;
+						*i_wk += g;
+						*i_jk += g;
+						*i_k += g;
+					}
+				}
+				j_jk+=K;
+			}
+	}
+
+	void learn() {
+		std::fill(n_wk_buf.begin(), n_wk_buf.end(), beta_);
+		std::fill(n_jk_buf.begin(), n_jk_buf.end(), alpha_);
+		std::fill(n_k_buf.begin(), n_k_buf.end(), beta_ * V_);
+
+		auto gamma_k = gamma_jik.begin();
+		auto j = docs_.begin(), jend = docs_.end();
+		auto j_jk = n_jk.begin();
+		auto j_jk_buf = n_jk_buf.begin();
+		for (;j!=jend;++j) {
+			auto i = j->begin(), iend = j->end();
+			for (;i!=iend;++i) {
+
+				size_t w = i->id;
+				int freq = i->freq;
+
+				update_for_word(
+					*gamma_k,
+					n_wk.begin(), j_jk, n_k.begin(),
+					n_wk_buf.begin(), j_jk_buf, n_k_buf.begin(),
+					w, freq, K_
+					);
+
+				++gamma_k;
+			}
+			j_jk += K_;
+			j_jk_buf += K_;
+
+		}
+
+
+
+		n_wk, n_wk_buf = n_wk_buf, n_wk;
+		n_jk, n_jk_buf = n_jk_buf, n_jk;
+		n_k, n_k_buf = n_k_buf, n_k;
+	}
 };
 
 } }
--- lda/ldacvb0_cpp/ldacvb0_test/test.cpp
+++ lda/ldacvb0_cpp/ldacvb0_test/test.cpp
@@ -3,6 +3,12 @@
 #include "ldacvb0.hpp"
 #include "cybozu/test.hpp"
 
+using cybozu::ldacvb0::Vec;
+using cybozu::ldacvb0::Mat;
+using cybozu::ldacvb0::Term;
+using cybozu::ldacvb0::Document;
+using cybozu::ldacvb0::Documents;
+
 CYBOZU_TEST_AUTO(test_vocabularies_for_std_string)
 {
 	std::string st =
@@ -26,6 +32,175 @@ CYBOZU_TEST_AUTO(test_vocabularies_for_std_string)
 	CYBOZU_TEST_EQUAL(v.count(","), 0);
 	CYBOZU_TEST_EQUAL(v.count("."), 0);
 	CYBOZU_TEST_EQUAL(v.count("np"), 0);
+}
+
+CYBOZU_TEST_AUTO(test_learn_update_parameter_for_word)
+{
+
+	const int K = 3, V = 3;
+
+	double GAMMA0[] = {0.2, 0.3, 0.5},
+		GAMMA2[] = {0.9, 0.05, 0.05};
+	double NWK[] = {1.1, 2.1, 3.1,  1.1, 1.1, 1.1,  2.1, 0.1, 0.1},
+		NJK[] = {4.2, 3.2, 4.2},
+		NK[] = {4.3, 3.3, 4.3};
+
+	Vec gamma0(&GAMMA0[0], &GAMMA0[K]);
+	Vec gamma2(&GAMMA2[0], &GAMMA2[K]);
+	Vec n_wk(&NWK[0], &NWK[V * K]), n_jk(&NJK[0], &NJK[K]), n_k(&NK[0], &NK[K]);
+	Vec n_wk_buf(V * K, 0.1), n_jk_buf(K, 0.2), n_k_buf(K, 0.3);
+
+
+	cybozu::ldacvb0::update_for_word(
+		gamma0,
+		n_wk_buf.begin(), n_jk_buf.begin(), n_k_buf.begin(),
+		n_wk.begin(), n_jk.begin(), n_k.begin(),
+		0, 1, K
+		);
+
+	CYBOZU_TEST_NEAR(gamma0[0], 0.17050723, 1e-6);
+	CYBOZU_TEST_NEAR(gamma0[1], 0.3378885 , 1e-6);
+	CYBOZU_TEST_NEAR(gamma0[2], 0.49160426, 1e-6);
+
+	CYBOZU_TEST_NEAR(n_wk_buf[0], 0.17050723 * 1 + 0.1, 1e-6);
+	CYBOZU_TEST_NEAR(n_wk_buf[1], 0.3378885  * 1 + 0.1, 1e-6);
+	CYBOZU_TEST_NEAR(n_wk_buf[2], 0.49160426 * 1 + 0.1, 1e-6);
+
+	CYBOZU_TEST_NEAR(n_jk_buf[0], 0.17050723 * 1 + 0.2, 1e-6);
+	CYBOZU_TEST_NEAR(n_jk_buf[1], 0.3378885  * 1 + 0.2, 1e-6);
+	CYBOZU_TEST_NEAR(n_jk_buf[2], 0.49160426 * 1 + 0.2, 1e-6);
+
+	CYBOZU_TEST_NEAR(n_k_buf[0], 0.17050723 * 1 + 0.3, 1e-6);
+	CYBOZU_TEST_NEAR(n_k_buf[1], 0.3378885  * 1 + 0.3, 1e-6);
+	CYBOZU_TEST_NEAR(n_k_buf[2], 0.49160426 * 1 + 0.3, 1e-6);
+
+		cybozu::ldacvb0::update_for_word(
+		gamma2,
+		n_wk_buf.begin(), n_jk_buf.begin(), n_k_buf.begin(),
+		n_wk.begin(), n_jk.begin(), n_k.begin(),
+		2, 3, K
+		);
+
+	CYBOZU_TEST_NEAR(gamma2[0], 0.922911 , 1e-6);
+	CYBOZU_TEST_NEAR(gamma2[1], 0.0384009, 1e-6);
+	CYBOZU_TEST_NEAR(gamma2[2], 0.0386877, 1e-6);
+
+	CYBOZU_TEST_NEAR(n_wk_buf[6], 0.922911  * 3 + 0.1, 1e-5);
+	CYBOZU_TEST_NEAR(n_wk_buf[7], 0.0384009 * 3 + 0.1, 1e-5);
+	CYBOZU_TEST_NEAR(n_wk_buf[8], 0.0386877 * 3 + 0.1, 1e-5);
+
+	CYBOZU_TEST_NEAR(n_jk_buf[0], 0.922911  * 3 + 0.17050723 * 1 + 0.2, 1e-5);
+	CYBOZU_TEST_NEAR(n_jk_buf[1], 0.0384009 * 3 + 0.3378885  * 1 + 0.2, 1e-5);
+	CYBOZU_TEST_NEAR(n_jk_buf[2], 0.0386877 * 3 + 0.49160426 * 1 + 0.2, 1e-5);
+
+	CYBOZU_TEST_NEAR(n_k_buf[0], 0.922911  * 3 + 0.17050723 * 1 + 0.3, 1e-5);
+	CYBOZU_TEST_NEAR(n_k_buf[1], 0.0384009 * 3 + 0.3378885  * 1 + 0.3, 1e-5);
+	CYBOZU_TEST_NEAR(n_k_buf[2], 0.0386877 * 3 + 0.49160426 * 1 + 0.3, 1e-5);
+}
+
+CYBOZU_TEST_AUTO(test_dirichlet_distribution)
+{
+	cybozu::ldacvb0::dirichlet_distribution dd(5489U);
+	Vec vec;
+	dd.draw(vec, 10, 0.1);
+	CYBOZU_TEST_EQUAL(vec.size(), 10);
+	CYBOZU_TEST_NEAR(vec[0]+vec[1]+vec[2]+vec[3]+vec[4]+vec[5]+vec[6]+vec[7]+vec[8]+vec[9], 1.0, 1e-5);
+	CYBOZU_TEST_NEAR(vec[0], 1.33922e-9,  1e-9);
+	CYBOZU_TEST_NEAR(vec[1], 7.01106e-10, 1e-15);
+	CYBOZU_TEST_NEAR(vec[2], 1.78974e-7,  1e-12);
+	CYBOZU_TEST_NEAR(vec[3], 5.01237e-11, 1e-16);
+	CYBOZU_TEST_NEAR(vec[4], 3.61902e-8,  1e-13);
+	CYBOZU_TEST_NEAR(vec[5], 0.664095,    1e-5);
+	CYBOZU_TEST_NEAR(vec[6], 1.64688e-10, 1e-15);
+	CYBOZU_TEST_NEAR(vec[7], 0.0692819,   1e-6);
+	CYBOZU_TEST_NEAR(vec[8], 4.03312e-24, 1e-29);
+	CYBOZU_TEST_NEAR(vec[9], 0.266623,    1e-5);
+
+}
+
+CYBOZU_TEST_AUTO(test_dirichlet_distribution2)
+{
+	cybozu::ldacvb0::dirichlet_distribution dd(1U);
+	double ALPHA[] = {1.0, 2.0, 3.0, 4.0};
+	Vec vec, alpha(&ALPHA[0], &ALPHA[4]);
+	dd.draw(vec, alpha);
+	CYBOZU_TEST_EQUAL(vec.size(), 4);
+	CYBOZU_TEST_NEAR(vec[0]+vec[1]+vec[2]+vec[3], 1.0, 1e-5);
+	CYBOZU_TEST_NEAR(vec[0], 0.0324463, 1e-6);
+	CYBOZU_TEST_NEAR(vec[1], 0.0198952, 1e-6);
+	CYBOZU_TEST_NEAR(vec[2], 0.673485,  1e-6);
+	CYBOZU_TEST_NEAR(vec[3], 0.274174,  1e-6);
+}
+
+CYBOZU_TEST_AUTO(test_lda_cvb0_initialization)
+{
+	Documents<std::string, char> docs;
+	docs.vocabularies.add("a");	// 0
+	docs.vocabularies.add("b");	// 1
+	docs.vocabularies.add("c");	// 2
+
+	int V = docs.vocabularies.size();
+	CYBOZU_TEST_EQUAL(V, 3);
+
+	{
+		docs.push_back(Document());
+		Document& doc = docs.back();
+		doc.push_back(Term(0, 2));
+		doc.push_back(Term(1, 1));
+	}
+	{
+		docs.push_back(Document());
+		Document& doc = docs.back();
+		doc.push_back(Term(0, 1));
+		doc.push_back(Term(2, 2));
+	}
+	{
+		docs.push_back(Document());
+		Document& doc = docs.back();
+		doc.push_back(Term(0, 1));
+		doc.push_back(Term(1, 1));
+		doc.push_back(Term(2, 1));
+	}
+	int K = 3, M = 3;
+	double alpha = 0.1, beta = 0.01;
+	cybozu::ldacvb0::LDA_CVB0 model(K, V, alpha, beta, docs);
+	CYBOZU_TEST_EQUAL(model.n_wk.size(), V * K);
+
+	auto &g = model.gamma_jik;
+	CYBOZU_TEST_EQUAL(g.size(), 7);
+	CYBOZU_TEST_EQUAL(g[0].size(), K);
+	CYBOZU_TEST_NEAR(g[0][0], 0.0, 1e-5);
+	CYBOZU_TEST_NEAR(g[0][1], 1.0, 1e-5);
+	CYBOZU_TEST_NEAR(g[0][2], 0.0, 1e-5);
+	CYBOZU_TEST_NEAR(g[1][0], 0.0, 1e-5);
+	CYBOZU_TEST_NEAR(g[1][1], 0.0, 1e-5);
+	CYBOZU_TEST_NEAR(g[1][2], 1.0, 1e-5);
+	CYBOZU_TEST_NEAR(g[2][0], 0.975824,  1e-5);
+	CYBOZU_TEST_NEAR(g[2][1], 0.0241761, 1e-5);
+	CYBOZU_TEST_NEAR(g[2][2], 0.0, 1e-5);
+	CYBOZU_TEST_NEAR(g[3][0], 1.0, 1e-5);
+	CYBOZU_TEST_NEAR(g[3][1], 0.0, 1e-5);
+	CYBOZU_TEST_NEAR(g[3][2], 0.0, 1e-5);
+	CYBOZU_TEST_NEAR(g[4][0], 0.999487, 1e-5);
+	CYBOZU_TEST_NEAR(g[4][1], 0.000513486, 1e-7);
+	CYBOZU_TEST_NEAR(g[4][2], 0.0, 1e-5);
+	CYBOZU_TEST_NEAR(g[5][0], 0.0, 1e-5);
+	CYBOZU_TEST_NEAR(g[5][1], 0.0, 1e-5);
+	CYBOZU_TEST_NEAR(g[5][2], 1.0, 1e-5);
+	CYBOZU_TEST_NEAR(g[6][0], 1.0, 1e-5);
+	CYBOZU_TEST_NEAR(g[6][1], 0.0, 1e-5);
+	CYBOZU_TEST_NEAR(g[6][2], 0.0, 1e-5);
+
+	CYBOZU_TEST_EQUAL(model.n_k.size(), K);
+	double vb = V * beta;
+	CYBOZU_TEST_NEAR(model.n_k[0], vb+g[0][0]*2+g[1][0]+g[2][0]+g[3][0]*2+g[4][0]+g[5][0]+g[6][0], 1e-6);
+	CYBOZU_TEST_NEAR(model.n_k[1], vb+g[0][1]*2+g[1][1]+g[2][1]+g[3][1]*2+g[4][1]+g[5][1]+g[6][1], 1e-6);
+	CYBOZU_TEST_NEAR(model.n_k[2], vb+g[0][2]*2+g[1][2]+g[2][2]+g[3][2]*2+g[4][2]+g[5][2]+g[6][2], 1e-6);
+
+	CYBOZU_TEST_EQUAL(model.n_jk.size(), M * K);
+	CYBOZU_TEST_NEAR(model.n_jk[0], alpha+g[0][0]*2+g[1][0], 1e-6);
+	CYBOZU_TEST_NEAR(model.n_jk[1], alpha+g[0][1]*2+g[1][1], 1e-6);
+	CYBOZU_TEST_NEAR(model.n_jk[2], alpha+g[0][2]*2+g[1][2], 1e-6);
 }
 
 /*
